c1f9a24f0b03e7b396b7c45e2c80eccd820614fe
Miguel Martinez <miguelitalia@hotmail.com>
2018-05-22 16:07:38 +0000

    started learning list comprenhensions

--- comp.py
+++ comp.py
@@ -1,9 +1,17 @@
 
 l = [1, 3, 5]
+p = [n*2 for n in l]
+print(p)
+
+# the above is the same as the below - this is a list conprenhension
 
 p = []
-
 for n in l:
     p.append(n * 2)
-    
 print(p)
+
+
+# chanllenge
+
+print([ i*2 for i in range(7)])
+
