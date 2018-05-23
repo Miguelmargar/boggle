bba015b15ec5a8c3fdca8f09ae1d29b24fac05fc
Miguel Martinez <miguelitalia@hotmail.com>
2018-05-23 15:39:27 +0000

    boggle sped up with including a set data structure

--- boggle.py
+++ boggle.py
@@ -48,8 +48,9 @@ def get_neighbours(pos):
 #     return neighbours_of
 
 # # this is the above in dictionay comprenhension   
-def all_grid_neighbours_dc(grid):
-    return {pos: [ n for n in neighbours if n in grid] for pos in grid}
+def all_grid_neighbours(grid):
+    return {pos: [ n for n in get_neighbours(pos) if n in grid] for pos in grid}
+#we use get_neighbours(pos) in the dictionary comprenhension and not neighbours as above as neighbours is specified above before needing it whereas in the dictionary comprenhension is used before it is created due to the syntax.    
     
 def path_to_word(grid, path):
     word = ""
@@ -64,5 +65,55 @@ def read_wordfile(filename):
     words = f.read().split("\n") # reads file
     words = [ w.upper() for w in words] #this is to make all words uppercase
     f.close() # closes file
-    return words
+    return set(words) 
+    # set is a different data structure which quickly says if something is in the dataset. a set data structure also removes duplicates. a list is slow in checking if something is included in the list specially if it is a big list as here we run into exponential growth in time needed to find all the posibilities if we increase the grid size so "set" helps with this. set makes it O1 and a list would be O n. 
+ 
+#this function searches    
+def search(grid, wordlist):
+    all_neighbours = all_grid_neighbours(grid)
+
+    def do_search(path, positions):
+        # If no more positions to search, there won't be any more words    
+        if positions == []:
+            return []
+        
+        # Extend the path by the first position and check if it's a word    
+        this_position = positions[0]
+        this_path = path + [this_position]
+        this_word = path_to_word(grid, this_path)
+        # Either it was a word, or it wasn't
+        if this_word in wordlist:
+            words = [this_word]
+        else:
+            words = []
+
+        # Find which neighbours of the current position, have not been used
+        # Search on extending the current path by the neighbours
+        neighbours = [n for n in all_neighbours[this_position] if n not in path]
+        words += do_search(this_path, neighbours)
+        
+        # Search on from the last path, using the siblings of the current position
+        words += do_search(path, positions[1:])
+        
+        # Return all the words found on this branch
+        return words
+            
+    return do_search([], list(grid.keys()))   
+
+
+#displays the words
+def display(words):
+    for word in words:
+        print(word)
+    print("Found %s words" % len(words))
+
+#main function putting everthing together
+def main():
+    grid = make_grid(3, 3)
+    dictionary = read_wordfile("bogwords.txt")
+    words = set(search(grid, dictionary))
+    display(words)
+
+if __name__ == "__main__": 
+    main()
     
